0dab46ccfaf603aa3dc040116ad6c16dbb87c3b1
Mario Schmidt <janus44444@gmail.com>
2012-05-24 14:22:47 +0200

    Added util.h with first utility functions moved to util.*

---
+++ utils.h
@@ -0,0 +1,12 @@
+#include "smatool.h"
+#ifndef TRUE
+  #define TRUE    1
+#endif
+#ifndef FALSE
+  #define FALSE    1
+#endif
+
+//
+unsigned char conv(char *nn);
+//
+int isSMA2plusPackage(unsigned char *package, int psize, int debug);
