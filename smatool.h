cc4c8638afb799720e29d2313f2e14078dffa6fb
Mario Schmidt <janus44444@gmail.com>
2012-05-24 09:52:46 +0200

    Adding header files

--- smatool.h
+++ smatool.h
@@ -1,8 +1,11 @@
 /*
  * smatool header file
- * /
+ */
 
-#define _XOPEN_SOURCE /* glibc needs this */
+// glibc may need this
+#ifndef _XOPEN_SOURCE
+  #define _XOPEN_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,11 +14,18 @@
 #include <bluetooth/rfcomm.h>
 #include <string.h>
 #include <math.h>
-#define __USE_XOPEN
+#ifndef __USE_XOPEN
+  #define __USE_XOPEN
+#endif
 #include <time.h>
 #include <assert.h>
 #include <sys/types.h>
 #include <curl/curl.h>
+#ifndef _GNU_SOURCE
+  #define _GNU_SOURCE
+#endif
+#include <getopt.h>
 
 // Beautify debugging
 #define debug_printf(...) if( debug == 1 ) { printf("%s line %d: ", __FILE__, __LINE__); printf (__VA_ARGS__); }
+#define verbose_printf(...) if( verbose == 1 ) { printf("Verbose: "); printf (__VA_ARGS__); }
